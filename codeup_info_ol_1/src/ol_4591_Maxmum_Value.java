c3aa29f4e085cb110147cf395183df09dd5fe503
2uijin <49094082+2uijin@users.noreply.github.com>
2020-02-09 03:21:41 +0900

    20200209_4591

---
+++ codeup_info_ol_1/src/ol_4591_Maxmum_Value.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class ol_4591_Maxmum_Value {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		
+		int num=0;
+		int max =0;
+		int idx =0;
+		
+		for(int i = 0; i<9; i++) {
+			num = sc.nextInt();
+			if(num>max) {
+				max=num;
+				idx = i;
+			}//if
+		}
+		
+		System.out.println(max);
+		System.out.println(idx+1);
+	}
+
+}
